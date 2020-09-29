d1f502af124c84afb25451d28778b5803187d732
Alan Ridlehoover <alan@ridlehoover.com>
2020-09-28 17:40:28 -0700

    Adds `default_resolver` and `resolves` class methods to Resolver.
    Co-authored-by: Fito von Zastrow <adolfovon@gmail.com>

--- lib/navigable/resolver.rb
+++ lib/navigable/resolver.rb
@@ -4,10 +4,23 @@ require 'navigable/observer_interface'
 
 module Navigable
   module Resolver
-    RESOLVE_NOT_IMPLEMENTED_MESSAGE = 'Class must implement `resolve` method.'
+    TYPE = :__resolver__
+    RESOLVE_NOT_IMPLEMENTED_MESSAGE = 'Resolver classes must implement a `resolve` method.'
 
     def self.extended(base)
+      base.extend(Manufacturable::Item)
       base.include(ObserverInterface)
+
+      base.instance_eval do
+        def default_resolver
+          default_manufacturable(TYPE)
+        end
+
+        def resolves(key)
+          corresponds_to(key, TYPE)
+        end
+      end
+
       base.class_eval do
         def resolve
           raise NotImplementedError.new(RESOLVE_NOT_IMPLEMENTED_MESSAGE)
--- spec/navigable/resolver_spec.rb
+++ spec/navigable/resolver_spec.rb
@@ -1,6 +1,36 @@
 RSpec.describe Navigable::Resolver do
   let(:resolver_klass) { Class.new { extend Navigable::Resolver } }
 
+  describe '.default_resolver' do
+    subject(:default_resolver) { resolver_klass.default_resolver }
+
+    before do
+      allow(resolver_klass).to receive(:default_manufacturable)
+
+      default_resolver
+    end
+
+    it 'delegates management of the default resolver to Manufacturable' do
+      expect(resolver_klass).to have_received(:default_manufacturable)
+    end
+  end
+
+  describe '.resolves' do
+    subject(:resolves) { resolver_klass.resolves(resolver_key) }
+
+    let(:resolver_key) { :resolver_key }
+
+    before do
+      allow(resolver_klass).to receive(:corresponds_to)
+
+      resolves
+    end
+
+    it 'delegates management of resolvers to Manufacturable' do
+      expect(resolver_klass).to have_received(:corresponds_to).with(resolver_key, Navigable::Resolver::TYPE)
+    end
+  end
+
   describe '.extended' do
     subject(:extended_object) { resolver_klass.new }
 
